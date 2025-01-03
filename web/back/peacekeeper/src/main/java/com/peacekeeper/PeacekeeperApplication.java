7022198838af146ebd635c69cad244ef515d4cbd
Benjamin Isasmendi <70007365+Mapacheee@users.noreply.github.com>
2025-01-03 15:41:20 -0300

    backend initial commit

---
+++ web/back/peacekeeper/src/main/java/com/peacekeeper/PeacekeeperApplication.java
@@ -0,0 +1,13 @@
+package com.peacekeeper.peacekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeacekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeacekeeperApplication.class, args);
+	}
+
+}
